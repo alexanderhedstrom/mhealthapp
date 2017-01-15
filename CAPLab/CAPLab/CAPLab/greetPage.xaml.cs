446eac7e6606c6463e4a062a2c35fd2cead998f3
Michael <miller.7594@osu.edu>
2017-01-15 02:41:36 -0500

    made a simple 'hello world' button

---
+++ CAPLab/CAPLab/CAPLab/greetPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CAPLab
+{
+    public partial class greetPage : ContentPage
+    {
+        public greetPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("CAPLab app", "Hello World", "Done.");
+        }
+    }
+}
