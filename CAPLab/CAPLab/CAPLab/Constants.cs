c26744cc3041a7fa15b24364e11de3a9b12dc2fb
AptFox <AptFox@users.noreply.github.com>
2017-01-28 16:32:45 -0500

    Implemented initial login page

---
+++ CAPLab/CAPLab/CAPLab/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPLab
+{
+    class Constants
+    {
+        public static string ParticipantID = "caplab";
+        public static string Password = "caplab";
+    }
+}
---
+++ CAPLab/CAPLab/CAPLab/Homepage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CAPLab
+{
+    // I intend to switch this to a TabbedPage once I figure out the syntax
+    // Each page will have to made a child of the primary tabbed navigation page. 
+    class Homepage : ContentPage
+    {
+        public Homepage()
+        {
+            //Consider implementing a logout function in the settings
+
+            Title = "Homepage";
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "This is the primary homepage",
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                    }
+                }
+            };
+        }
+    }
+}
