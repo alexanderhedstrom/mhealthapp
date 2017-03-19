670ed3e82eb47459d28b2fec2abb7f272f6673f1
AptFox <AptFox@users.noreply.github.com>
2017-03-18 22:23:01 -0400

    added links to API implementation for Xamarin in API classes

--- CAPLab/CAPLab/CAPLab.Droid/APIConnector.cs
+++ CAPLab/CAPLab/CAPLab.Droid/APIConnector.cs
@@ -25,7 +25,10 @@ namespace CAPLab.Droid
         // https://developer.xamarin.com/guides/cross-platform/application_fundamentals/web_services/#rest
 
         HttpClient client;
-
+        //https://developer.xamarin.com/api/type/System.Net.Http.HttpClient/
+        //https://developer.xamarin.com/guides/xamarin-forms/cloud-services/consuming/rest/
+        //https://developer.xamarin.com/recipes/android/web_services/consuming_services/call_a_rest_web_service/
+        //https://developer.xamarin.com/guides/cross-platform/application_fundamentals/web_services/#rest
         public APIConnector()
         {
 
--- CAPLab/CAPLab/CAPLab.iOS/APIConnector.cs
+++ CAPLab/CAPLab/CAPLab.iOS/APIConnector.cs
@@ -15,7 +15,10 @@ namespace CAPLab.iOS
         // https://developer.xamarin.com/guides/cross-platform/application_fundamentals/web_services/#rest
 
         HttpClient client;
-
+        //https://developer.xamarin.com/api/type/System.Net.Http.HttpClient/
+        //https://developer.xamarin.com/guides/xamarin-forms/cloud-services/consuming/rest/
+        //https://developer.xamarin.com/recipes/android/web_services/consuming_services/call_a_rest_web_service/
+        //https://developer.xamarin.com/guides/cross-platform/application_fundamentals/web_services/#rest
         public APIConnector()
         {
 
